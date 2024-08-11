65c020217d36fc682d19ef370a17f0963e4c351d
Sung <sungjincho92@gmail.com>
2024-08-10 22:29:17 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,112 @@
 import Image from "next/image";
+import { promises as fs } from "fs";
 
-export default function Home() {
+export default async function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="bg-main-color desktop:bg-[#ffff] desktop:grid-cols-2 desktop:rounded-[30px] grid max-w-[700px]">
+      <Performance />
+      <Summary />
     </main>
   );
 }
+
+// PERFORMANCE, grid 1 / 2//
+const Performance = () => {
+  return (
+    <div className="desktop:py-[40px] desktop:px-[30px] desktop:rounded-[30px] text-wrap rounded-bl-[30px] rounded-br-[30px] bg-gradient-to-t from-gradients-royalblue to-gradients-slateblue px-[20px] py-[25px] text-center ">
+      <h2 className="desktop:text-[24px] text-[18px] font-medium text-neutral-lightlavender">
+        Your Result
+      </h2>
+      <p className="desktop:mt-[85px] desktop:text-[72px] desktop:before:w-[170px] desktop:before:h-[170px] desktop:font-bold relative z-10 mt-[65px] text-[56px] leading-[45px] text-[#ffff] content-none before:absolute before:left-[50%] before:top-[50%] before:z-[-1] before:block before:h-[150px] before:w-[150px] before:translate-x-[-50%] before:translate-y-[-60%] before:rounded-full before:bg-gradient-to-b before:from-gradients-circle-violetblue/100 before:to-gradients-circle-persianblue/0">
+        76<span className="block text-[16px] text-[#c8c7ff]/50">of 100</span>
+      </p>
+      <p className="desktop:text-[32px] desktop:font-bold mt-[40px] text-[24px] text-[#ffff]">
+        Great
+      </p>
+      <p className="desktop:mt-[17px] desktop:text-[18px] desktop:text-pretty mb-[10px] mt-[5px] text-[16px] text-neutral-lightlavender">
+        Your performance exceed 65% of the people conducting the test here!
+      </p>
+    </div>
+  );
+};
+
+type DataItem = {
+  category: string;
+  score: string;
+};
+
+type FetchedData = DataItem[];
+
+// SUMMARY, grid 2 / 2//
+const Summary = async () => {
+  const res = await fs.readFile(process.cwd() + "/data/data.json", "utf8");
+  const data: FetchedData = JSON.parse(res);
+  // const res = await fetch("http://localhost:4000/data");
+  // const data: FetchedData = await res.json();
+
+  return (
+    <div className="desktop:px-[35px] desktop:py-[40px] px-[25px] py-[30px] ">
+      <h2 className="desktop:text-[24px] font-neutral-grayblue text-[18px] font-bold">
+        Summary
+      </h2>
+      <div className="mt-[30px] flex flex-col gap-[15px]">
+        {data
+          ? data.map((el) => <Item el={el} key={el.category} />)
+          : "error fetching"}
+      </div>
+      <a
+        className="mt-[25px] block rounded-full bg-gradient-to-t from-neutral-grayblue to-neutral-grayblue p-[16px] text-center text-[#ffff] hover:bg-gradient-to-t hover:from-gradients-royalblue hover:to-gradients-slateblue"
+        href="#"
+      >
+        Continue
+      </a>
+    </div>
+  );
+};
+
+type ItemProps = {
+  el: DataItem;
+};
+
+const Item = ({ el }: ItemProps) => {
+  let bgcolor;
+  let textcolor;
+  if (el.category === "Reaction") {
+    bgcolor = "bg-primary-lightred/[.05]";
+    textcolor = "text-primary-lightred";
+  }
+  if (el.category === "Memory") {
+    bgcolor = "bg-primary-orangeyellow/[.05]";
+    textcolor = "text-primary-orangeyellow";
+  }
+  if (el.category === "Verbal") {
+    bgcolor = "bg-primary-greenteal/[.05]";
+    textcolor = "text-primary-greenteal";
+  }
+  if (el.category === "Visual") {
+    bgcolor = "bg-primary-cobaltblue/[.05]";
+    textcolor = "text-primary-cobaltblue";
+  }
+
+  return (
+    <>
+      <div
+        className={`${bgcolor} flex place-content-between rounded-[10px] px-[12px] py-[15px]`}
+      >
+        <div className="flex gap-[15px]">
+          <Image
+            src={`/assets/images/icon-${el.category}.svg`}
+            alt="summary icon"
+            width={20}
+            height={20}
+          ></Image>
+          <p className={textcolor}>{el.category}</p>
+        </div>
+        <p className="font-neutral-grayblue font-extrabold">
+          {el.score}
+          <span className="font-medium opacity-50"> / 100</span>
+        </p>
+      </div>
+    </>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,29 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: {
+        lightred: "hsl(0, 100%, 67%)",
+        orangeyellow: "hsl(39, 100%, 56%)",
+        greenteal: "hsl(166, 100%, 37%)",
+        cobaltblue: "hsl(234, 85%, 45%)",
+      },
+      gradients: {
+        slateblue: "hsl(252, 100%, 67%)",
+        royalblue: "hsl(241, 81%, 54%)",
+        circle: {
+          violetblue: "hsl(256, 72%, 46%)", // /1
+          persianblue: "hsl(241, 72%, 46%)", // /0
+        },
+      },
+      neutral: {
+        paleblue: "hsl(221, 100%, 96%)",
+        lightlavender: "hsl(241, 100%, 89%)",
+        grayblue: "hsl(224, 30%, 27%)",
       },
     },
+    screens: { desktop: "700px" },
+    extend: {},
   },
   plugins: [],
 };
